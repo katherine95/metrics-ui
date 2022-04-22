8a1fa77925c6d0ac6f69bcb5ecd91733d5e6f934
katherine95 <kathiekim95@gmail.com>
2022-04-22 10:29:19 +0300

    remove commented code

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Metrics from "./views";
 import AddMetric from "./views/addMetric";
-// import TriviaQuiz from "./views/quiz";
-// import Results from "./views/results";
 
 const MetricsAppRoutes = () => {
   return (
@@ -11,8 +9,6 @@ const MetricsAppRoutes = () => {
       <Routes>
         <Route path="/" element={<Metrics />} exact />
         <Route path="/add-metric" element={<AddMetric />} exact />
-        {/* <Route path="/results" element={<Results />} exact />
-        <Route path="*" element={<Navigate to="/" replace />} /> */}
       </Routes>
     </BrowserRouter>
   );
